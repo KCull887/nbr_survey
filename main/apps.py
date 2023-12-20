b6207b7b5e116e15dc5137d6facd6f1b81716a7e
John Meinken <thebellsofohio@hotmail.com>
2023-12-20 16:43:52 -0500

    make sure setting VISIT_INFO_CUTOFF_DATE is set when the application starts

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    # make sure that required custom settings are in place
+    if not hasattr(settings, "VISIT_INFO_CUTOFF_DATE"):
+        raise AttributeError("VISIT_INFO_CUTOFF_DATE is required in your Django settings.")
